5609afc823bffb9e060f3d6e970923a64ac9fe40
Seb <seb@dsebastien.net>
2021-03-02 18:38:25 +0100

    Changed l&f of blog-post-summary

--- apps/website/client/components/blog-post-summary.tsx
+++ apps/website/client/components/blog-post-summary.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Link from 'next/link';
 
 import { FrontMatter } from '@/lib/front-matter.intf';
@@ -15,13 +16,15 @@ const BlogPostSummary = ({ title, summary, slug }: BlogPostSummaryProps) => {
   return (
     <Link href={`/blog/${slug}`}>
       <a className="w-full">
-        <div className="mb-8 w-full">
-          <div className="flex flex-col md:flex-row justify-between">
-            <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
-              {title}
-            </h4>
+        <div className="border-2 rounded-lg p-2 hover:border-gray-600">
+          <div className="mb-4 w-full">
+            <div className="flex flex-col md:flex-row justify-between">
+              <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
+                {title}
+              </h4>
+            </div>
+            <p className="text-gray-600 dark:text-gray-400">{summary}</p>
           </div>
-          <p className="text-gray-600 dark:text-gray-400">{summary}</p>
         </div>
       </a>
     </Link>
