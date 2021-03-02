f92f96f66d19127227476ceed3686f693a5c1dc6
Seb <seb@dsebastien.net>
2021-03-02 18:42:20 +0100

    Added 404 page

---
+++ apps/website/pages/404.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+import Layout from '@/layouts/layout';
+
+const NotFound = () => {
+  return (
+    <Layout
+      customMeta={{
+        title: '404 Page not found – Sébastien Dubois',
+      }}
+    >
+      <div className="max-w-2xl mx-auto">
+        <h1 className="page-heading">Doh...</h1>
+        <p className="text-gray-600 dark:text-gray-400 mb-8">
+          You seem to be lost. Hold my hand (yes, it's safe here), and let me
+          show you the way.
+        </p>
+        <Link href="/">
+          <a className="site-button">Go back home</a>
+        </Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFound;
