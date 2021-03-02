eeb10d6754622eb9eb8c5b6741a60626584f0029
Seb <seb@dsebastien.net>
2021-03-02 14:32:50 +0100

    Added jest config

---
+++ apps/website/jest.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  displayName: 'website',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      { cwd: __dirname, configFile: './babel-jest.config.json' },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/website',
+};
---
+++ jest.preset.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const nxPreset = require('@nrwl/jest/preset');
+
+module.exports = { ...nxPreset };
