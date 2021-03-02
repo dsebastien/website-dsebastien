544a955e01e1bd0317c992f9f12800d24eeed0cb
Seb <seb@dsebastien.net>
2021-03-02 17:43:41 +0100

    Added blot post summary component

---
+++ apps/website/client/components/blog-post-summary.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+import { FrontMatter } from '@/lib/front-matter.intf';
+
+type BlogPostSummaryProps = Pick<FrontMatter, 'title' | 'summary' | 'slug'>;
+
+/**
+ * Short summary of a blog post
+ * @param title
+ * @param summary
+ * @param slug
+ * @constructor
+ */
+const BlogPostSummary = ({ title, summary, slug }: BlogPostSummaryProps) => {
+  return (
+    <Link href={`/blog/${slug}`}>
+      <a className="w-full">
+        <div className="mb-8 w-full">
+          <div className="flex flex-col md:flex-row justify-between">
+            <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
+              {title}
+            </h4>
+          </div>
+          <p className="text-gray-600 dark:text-gray-400">{summary}</p>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default BlogPostSummary;
