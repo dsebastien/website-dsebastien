a7e13067200372b014dab5531769d9601eda575e
Seb <seb@dsebastien.net>
2021-03-03 09:26:58 +0100

    Switched to next links

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '@/layouts/layout';
+import Link from 'next/link';
 
 const IndexPage = () => {
   return (
@@ -8,7 +9,7 @@ const IndexPage = () => {
         <h1 className="page-heading">Hi, I'm Sébastien Dubois 👋</h1>
         <p className="text-gray-900 dark:text-gray-400">
           There's not much to see here for now, but check out{' '}
-          <a href="/blog">my blog</a>, and the <a href="/about">about page</a>.
+          <Link href="/blog">my blog</Link>, and the <Link href="/about">about page</Link>.
         </p>
       </div>
     </Layout>
