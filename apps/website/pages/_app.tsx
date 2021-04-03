4c9e0c815a2535a90bbe836471b2d85611a0409e
Seb <seb@dsebastien.net>
2021-04-03 12:38:04 +0200

    Fixed problem with head

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -7,6 +7,7 @@ import GlobalStylesComponent from '@/components/global-styles';
 import { MDXProvider } from '@mdx-js/react';
 import MDXComponents from '@/components/mdx-components';
 import useRouterScroll from '../utils/use-router-scroll';
+import Head from 'next/head';
 
 /**
  * Wrapper around all pages
@@ -22,6 +23,10 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
+      <Head>
+        {/* Why here? https://nextjs.org/docs/messages/no-document-viewport-meta */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <GlobalStylesComponent />
       {/* Use the ThemeProvider of next-themes, combined with Tailwind: https://github.com/pacocoursey/next-themes#with-tailwind */}
       <ThemeProvider attribute="class">
