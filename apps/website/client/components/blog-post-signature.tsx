2472d053242f97307c3448f7b7f51d25dcee2cb0
Sebastien Dubois <seb@dsebastien.net>
2021-09-05 23:51:15 +0200

    Updated signature

--- apps/website/client/components/blog-post-signature.tsx
+++ apps/website/client/components/blog-post-signature.tsx
@@ -13,14 +13,10 @@ const BlogPostSignature = () => {
   return (
     <>
       <StyledDiv className="mb-2">
-        PS: If you want to learn tons of other cool things about
-        product/software/Web development, then{' '}
-        <a href="https://dev-concepts.dev" rel="noopener">
-          check out the Dev Concepts series of books
-        </a>
+        PS: <a href="https://dev-concepts.dev" rel="noopener">
+          check out my Dev Concepts books</a>
         ,{' '}<a href="https://join.slack.com/t/softwarecrafterstalk/shared_invite/zt-umgx3v06-4rtJ20PXz867GTPzCk1zeQ" rel="noopener">
-        join the Software Crafters community
-      </a>
+        join the Software Crafters community</a>, the <a href="https://dsebastien.net/pkm-community" rel="noopener">Personal Knowledge Management community</a>
         , and{' '}
         <a href="https://twitter.com/dSebastien" rel="noopener">
           come say hi on Twitter!
