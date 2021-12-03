51b89c523ec2cc62f8d84f58928cae25c02d782f
Sebastien Dubois <seb@dsebastien.net>
2021-12-03 16:24:01 +0100

    Updated signature to link to PKM community introduction article

--- apps/website/client/components/blog-post-signature.tsx
+++ apps/website/client/components/blog-post-signature.tsx
@@ -15,7 +15,7 @@ const BlogPostSignature = () => {
       <StyledDiv className="mb-2">
         PS: <a href="https://dev-concepts.dev" rel="noopener">
           check out the Dev Concepts collection of e-books</a>
-        ,{' '}<a href="https://join.slack.com/t/softwarecrafterstalk/shared_invite/zt-umgx3v06-4rtJ20PXz867GTPzCk1zeQ" rel="noopener">
+        ,{' '}<a href="https://dsebastien.net/blog/2021-11-12-personal-knowledge-management-community" rel="noopener">
         join the Software Crafters community</a>, the <a href="https://dsebastien.net/pkm-community" rel="noopener">Personal Knowledge Management community</a>
         , and{' '}
         <a href="https://twitter.com/dSebastien" rel="noopener">
