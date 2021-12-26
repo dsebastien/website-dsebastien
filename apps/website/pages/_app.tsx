b14fa5c58d40f52cb85fdfe2b9210b11ef2bdff6
Sebastien Dubois <seb@dsebastien.net>
2021-12-26 01:01:51 +0100

    Added gumroad script

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -27,6 +27,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         {/* Why here? https://nextjs.org/docs/messages/no-document-viewport-meta */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script src="https://gumroad.com/js/gumroad.js"></script>
       </Head>
       <GlobalStylesComponent />
       {/* Plausible Analytics. Reference: https://plausible.io/docs/nextjs-integration */}
