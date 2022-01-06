c63add516765a6b7eae929c2c604a9f7fd7ac5fc
Sebastien Dubois <seb@dsebastien.net>
2022-01-06 10:55:46 +0100

    Updated home page

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -12,7 +12,7 @@ const IndexPage = () => {
       <StyledDiv className="article-content-wrapper">
         <h1 className="page-heading">Hello! <span className="animate-wave ">👋</span></h1>
         <p className="text-gray-900 dark:text-gray-400 text-5xl	tracking-wide leading-normal">
-          I'm <span className="font-bold text-gray-900 dark:text-gray-100">Sébastien Dubois</span>, <span className="font-bold text-gray-900 dark:text-gray-100">author ✍️</span>, <span className="font-bold text-gray-900 dark:text-gray-100">founder 🚀</span>, <span className="font-bold text-gray-900 dark:text-gray-100">CTO 🔨</span> & <span className="font-bold text-gray-900 dark:text-gray-100">bootstrapper ✨</span>.
+          I'm <span className="font-bold text-gray-900 dark:text-gray-100">Sébastien Dubois</span>, <span className="font-bold text-gray-900 dark:text-gray-100">Author</span> ✍, <span className="font-bold text-gray-900 dark:text-gray-100">Founder</span> 🚀, <span className="font-bold text-gray-900 dark:text-gray-100">CTO</span> 🔨 & <span className="font-bold text-gray-900 dark:text-gray-100">Indie Hacker</span> ✨.
         </p>
         <p className="text-3xl text-gray-900 dark:text-gray-400">
           Want to get in touch? 👉 <span className="font-bold"><Link href="https://twitter.com/dSebastien">@dSebastien</Link></span>
