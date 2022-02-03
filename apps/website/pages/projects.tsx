6965af5a449157ea0d2305cbe2bb08f7f87b90eb
Sebastien Dubois <seb@dsebastien.net>
2022-02-03 11:48:00 +0100

    Updated projects list

--- apps/website/pages/projects.tsx
+++ apps/website/pages/projects.tsx
@@ -24,24 +24,7 @@ const ProjectsPage = () => {
 
         <section className="mb-8 text-gray-900 dark:text-gray-400 flex grid grid-cols-1 sm:grid-cols-2 grid-flow-row items-center gap-6">
 
-          <a href="https://developassion.gumroad.com/l/PersonalKnowledgeManagementLibrary" rel="noopener">
-            <article className="project-card">
-              <div>
-                <Image
-                  alt="Personal Knowledge Management Library"
-                  width={132}
-                  height={132}
-                  src="/static/images/logos/pkm-library.svg"
-                  className="rounded-md"
-                />
-              </div>
-              <footer>
-                <span>A collection of curated resources about Personal Knowledge Management (PKM)</span>
-              </footer>
-            </article>
-          </a>
-
-          <a href="https://focusd.app" rel="noopener">
+        <a href="https://focusd.app" rel="noopener">
             <article className="project-card">
               <div>
                 <Image
@@ -58,19 +41,19 @@ const ProjectsPage = () => {
             </article>
           </a>
 
-          <a href="https://dsebastien.net/blog/2021-08-25-software-crafters-community" rel="noopener">
+          <a href="https://developassion.gumroad.com/l/PersonalKnowledgeManagementLibrary" rel="noopener">
             <article className="project-card">
               <div>
                 <Image
-                  alt="Software Crafters Community"
+                  alt="Personal Knowledge Management Library"
                   width={132}
                   height={132}
-                  src="/static/images/projects/community.svg"
+                  src="/static/images/logos/pkm-library.svg"
                   className="rounded-md"
                 />
               </div>
               <footer>
-                <span>An online community for Software Crafters</span>
+                <span>PKM Library: A collection of curated resources about Personal Knowledge Management (PKM)</span>
               </footer>
             </article>
           </a>
@@ -92,9 +75,78 @@ const ProjectsPage = () => {
             </article>
           </a>
 
+          <a href="https://www.reddit.com/r/PersonalKnowledgeMgmt/" rel="noopener">
+            <article className="project-card">
+              <div>
+                <Image
+                  alt="Personal Knowledge Management subreddit"
+                  width={132}
+                  height={132}
+                  src="/static/images/logos/reddit.svg"
+                  className="rounded-md"
+                />
+              </div>
+              <footer>
+                <span>A subreddit dedicated to Personal Knowledge Management (PKM)</span>
+              </footer>
+            </article>
+          </a>
+
+          <a href="https://pkmjournal.com" rel="noopener">
+            <article className="project-card">
+              <div>
+                <Image
+                  alt="Personal Knowledge Management Journal"
+                  width={132}
+                  height={132}
+                  src="/static/images/projects/pkm-journal.png"
+                  className="rounded-md"
+                />
+              </div>
+              <footer>
+                <span>PKM Journal: A publication dedicated to Personal Knowledge Management (PKM)</span>
+              </footer>
+            </article>
+          </a>
+
+          <a href="https://personal-knowledge-management.com" rel="noopener">
+            <article className="project-card">
+              <div>
+                <Image
+                  alt="Personal Knowledge Management Homepage"
+                  width={132}
+                  height={132}
+                  src="/static/images/logos/pkm-library.svg"
+                  className="rounded-md"
+                />
+              </div>
+              <footer>
+                <span>Personal Knowledge Management (PKM) homepage</span>
+              </footer>
+            </article>
+          </a>
+
+          <a href="https://dsebastien.net/blog/2021-08-25-software-crafters-community" rel="noopener">
+            <article className="project-card">
+              <div>
+                <Image
+                  alt="Software Crafters Community"
+                  width={132}
+                  height={132}
+                  src="/static/images/logos/slack.svg"
+                  className="rounded-md"
+                />
+              </div>
+              <footer>
+                <span>An online community for Software Crafters</span>
+              </footer>
+            </article>
+          </a>
+
           <a href="https://stark.nbb.be" rel="noopener">
             <article className="project-card">
               <div>
+              <div className="h-6"></div>
                 <Image
                   alt="Stark Framework"
                   width={345}
@@ -102,6 +154,7 @@ const ProjectsPage = () => {
                   src="/static/images/projects/stark.svg"
                   className="rounded-md"
                 />
+                <div className="h-6"></div>
               </div>
               <footer>
                 <span>An Open Source Enterprise front-end framework on top of Angular</span>
@@ -112,13 +165,13 @@ const ProjectsPage = () => {
           <a href="https://github.com/NationalBankBelgium/REST-API-Design-Guide/wiki" rel="noopener">
             <article className="project-card">
               <div>
-                <Image
-                  alt="REST API Design Guide"
-                  width={345}
-                  height={100}
-                  src="/static/images/projects/rest.png"
-                  className="rounded-md"
-                />
+                  <Image
+                    alt="REST API Design Guide"
+                    width={345}
+                    height={100}
+                    src="/static/images/projects/rest.png"
+                    className="rounded-md"
+                  />
               </div>
               <footer>
                 <span>A comprehensive Open Source design guide for RESTful APIs</span>
