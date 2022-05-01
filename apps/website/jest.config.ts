c84537230b4f99c8db92c8d071e9aee2701098ca
Sebastien Dubois <seb@dsebastien.net>
2022-05-01 17:40:17 +0200

    Updated NX

--- apps/website/jest.config.ts
+++ apps/website/jest.config.ts
@@ -2,14 +2,12 @@
 
 module.exports = {
   displayName: 'website',
-  preset: '../../jest.preset.js',
+
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': [
-      'babel-jest',
-      { cwd: __dirname, configFile: './babel-jest.config.json' },
-    ],
+    '^.+\\.[tj]sx?$': ['babel-jest', { cwd: __dirname, configFile: './babel-jest.config.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/website',
+  preset: '../../jest.preset.ts',
 };
