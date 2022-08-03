e04ad5949d034236d0c8d5715da52178adb24bd5
Sebastien Dubois <seb@dsebastien.net>
2022-08-03 16:44:59 +0200

    Updated pkm signature

--- apps/website/client/components/pkm-signature.tsx
+++ apps/website/client/components/pkm-signature.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TwitterFollowButton } from 'mdx-embed';
-import NewsletterSubscribe from "@/components/newsletter-subscribe";
+import NewsletterSubscribe from '@/components/newsletter-subscribe';
 
 /**
  * Signature for my blog posts
@@ -11,14 +11,23 @@ const PKMSignature = () => {
     <>
       <h2>About Sébastien</h2>
       <div className="mb-2">
-      Hello everyone! I'm Sébastien Dubois. I'm an author, founder, and CTO. I write books and articles about software development & IT, personal knowledge management, personal organization, and productivity. I also craft lovely digital products 🚀
-      <br /><br />
-      If you've enjoyed this article and want to read more like this, then subscribe to <a href="https://newsletter.dsebastien.net">my newsletter</a>, check out my <a href="https://developassion.gumroad.com/l/PersonalKnowledgeManagementLibrary">PKM Library</a>, and the <a href="https://developassion.gumroad.com/l/obsidian-starter-kit">Obsidian Starter Kit</a> 🔥.
-      <br /><br />
-      You can <a href="https://twitter.com/dSebastien">follow me on Twitter</a> 🐦
-      <br /><br />
-      If you want to discuss, then don't hesitate to join the <a href="https://dsebastien.net/pkm-community" rel="noopener">Personal Knowledge Management community</a>
-
+        Hello everyone! I'm Sébastien Dubois. I'm an author, founder, and CTO. I write books and articles about personal knowledge
+        management (PKM), personal organization, software development & IT, and productivity. I also craft lovely digital products 🚀
+        <br />
+        <br />
+        If you've enjoyed this article and want to read more, then subscribe to{' '}
+        <a href="https://newsletter.dsebastien.net">my newsletter</a>, check out my{' '}
+        <a href="https://developassion.gumroad.com/l/PersonalKnowledgeManagementLibrary">PKM Library</a>, and the{' '}
+        <a href="https://developassion.gumroad.com/l/obsidian-starter-kit">Obsidian Starter Kit</a> 🔥.
+        <br />
+        <br />
+        You can <a href="https://twitter.com/dSebastien">follow me on Twitter</a> 🐦
+        <br />
+        <br />
+        If you want to discuss, then don't hesitate to join the{' '}
+        <a href="https://dsebastien.net/pkm-community" rel="noopener">
+          Personal Knowledge Management community
+        </a>
         <div className="mt-6">
           <NewsletterSubscribe />
         </div>
